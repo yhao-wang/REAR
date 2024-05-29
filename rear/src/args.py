6676ae002374c92e9021422d1d6b4a60dd04f749
Yuhao Wang <107921246+yhao-wang@users.noreply.github.com>
2024-05-29 14:13:26 +0800

    Update args.py

--- rear/src/args.py
+++ rear/src/args.py
@@ -23,26 +23,21 @@ class RearArguments:
     )
     psg_num: Optional[int] = field(
         default=8,
-        metadata={"help": "The alpha parameter to control the noise magnitude in NEFTune."}
     )
     is_warm_up: Optional[bool] = field(
         default=False,
     )
     minor_diff: Optional[float] = field(
         default=0.0,
-        metadata={"help": "The alpha parameter to control the noise magnitude in NEFTune."}
     )
     head_scaler: Optional[float] = field(
         default=1.0,
-        metadata={"help": "The scale factor for LoRA fine-tuning (similar with the learning rate)."}
     )
     proj_scaler: Optional[float] = field(
         default=1.0,
-        metadata={"help": "The scale factor for LoRA fine-tuning (similar with the learning rate)."}
     )
     bce_bias: Optional[float] = field(
         default=0.5,
-        metadata={"help": "The scale factor for LoRA fine-tuning (similar with the learning rate)."}
     )
 
 
